c6a856b479bceb3e76c5481d4c40951bc9b69f8f
k3dsyam <45802925+k3dsyam@users.noreply.github.com>
2021-05-13 18:09:17 -0400

    Add files via upload

---
+++ js/navigation.js
@@ -0,0 +1,14 @@
+const scrollToTopBtn = document.querySelector(".scroll-to-top-btn");
+const headerBottom = document.querySelector(".header-bottom");
+const stickyOffset = headerBottom.offsetTop;
+
+document.addEventListener("scroll", (e) => {
+  if (window.scrollY >= stickyOffset) {
+    headerBottom.classList.add("fixed");
+    scrollToTopBtn.classList.add("show");
+  } else {
+    headerBottom.classList.remove("fixed");
+    scrollToTopBtn.classLisemove("show");
+  }
+});
+
